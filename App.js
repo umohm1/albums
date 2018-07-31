00fd1c593517dd7919bee65ad731482ad66d3621
Mary <mumoh@mozilla.com>
2018-07-30 20:47:58 -0400

    Begin building out index.js and App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Header from './src/components/header';
+
+const App = () => (
+  <Header />
+);
+
+
+export default App; 

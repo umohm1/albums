6108e69cdea589a7409305aca6579eae0ab24346
Mary <mumoh@mozilla.com>
2018-07-30 21:14:10 -0400

    Add header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-  return <Text>Albums!</Text>;
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>;
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  },
+  textStyle: {
+    fontSize: 20
+  }
 };
 
 export default Header;

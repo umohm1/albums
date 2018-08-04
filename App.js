17927d18845bec7cdef13d51bce55d3b27e13b0f
Mary <mumoh@mozilla.com>
2018-08-04 13:38:20 -0400

    Add AlbumList component

--- App.js
+++ App.js
@@ -5,10 +5,15 @@
  */
 
 import React, { Component } from 'react';
-import Header from './src/components/header';
+import {View} from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-  <Header headerText={'Albums'}/>
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 
